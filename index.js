cb953b2ac66fa48e5c297cd826703724153b009b
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2021-07-19 21:24:39 -0400

    Update JSDoc

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
 /**
+ * ...
+ * @typedef {Object} FixConfig
+ * @property {string} type
+ * @property {string} value
+ */
+
+/**
+ * ...
+ * @typedef {Object} ExpConfig
+ * @property {string} type
+ * @property {string} value
+ */
+
+/**
+ * ...
+ * @typedef {Object} ExpIfConfig
+ * @property {string} type
+ * @property {string} condition
+ * @property {Config} trueValue
+ * @property {Config} falseValue
+ */
+
+/**
+ * ...
+ * @typedef {Object} ExpMapConfig
+ * @property {string} type
+ * @property {string} array
+ * @property {string[]} arguments
+ * @property {Config} value
+ * @property {string} joinString
+ */
+
+/**
+ * ...
+ * @typedef {Object} CatConfig
+ * @property {string} type
+ * @property {Config[]} values
+ */
+
+/**
+ * ...
+ * @typedef {Object} TagConfig
+ * @property {string} type
+ * @property {Object} name
+ * @property {Object} attributes
+ * @property {Object} body
+ */
+
+/**
+ * ...
+ * @typedef {Object} FuncConfig
+ * @property {string} type
+ * @property {string[]} arguments
+ * @property {Config} value
+ */
+
+/**
+ * ...
+ * @typedef {(FixConfig|ExpConfig|ExpIfConfig|ExpMapConfig|CatConfig|TagConfig|FuncConfig)} Config
+ */
+
+/**
  * Represents a fix string.
  * @param {string} value
- * @returns {Object}
+ * @returns {FixConfig}
  */
 function fix(value) {
 	return { type: 'fix', value };
@@ -10,20 +72,20 @@ function fix(value) {
 /**
  * Represents an expression.
  * @param {string} value
- * @returns {Object}
+ * @returns {ExpConfig}
  */
-function exp(value) {
+ function exp(value) {
 	return { type: 'exp', value };
 }
 
 /**
  * Represents an "if" expression.
  * @param {string} condition
- * @param {Object} trueValue
- * @param {Object} falseValue
- * @returns {Object}
+ * @param {Config} trueValue
+ * @param {Config} falseValue
+ * @returns {ExpIfConfig}
  */
-function expIf(condition, trueValue, falseValue) {
+ function expIf(condition, trueValue, falseValue) {
 	return { type: 'expIf', condition, trueValue, falseValue };
 }
 
@@ -31,18 +93,18 @@ function expIf(condition, trueValue, falseValue) {
  * Represents a "Array.map" expression.
  * @param {string} array
  * @param {string[]} arguments
- * @param {Object} value
+ * @param {Config} value
  * @param {string} joinString
- * @returns {Object}
+ * @returns {ExpMapConfig}
  */
 function expMap(array, arguments, value, joinString) {
 	return { type: 'expMap', array, arguments, value, joinString };
 }
 
 /**
- * Concatinate.
- * @param {...Object} values
- * @returns {Object}
+ * Concatinate configurations.
+ * @param  {...Config} values
+ * @returns {CatConfig}
  */
 function cat(...values) {
 	return { type: 'cat', values };
@@ -50,42 +112,40 @@ function cat(...values) {
 
 /**
  * Represents an HTML tag.
- * @param {Object} name
- * @param {Object} attributes
- * @param {Object} body
- * @returns {Object}
+ * @param {Config} name
+ * @param {Config} attributes
+ * @param {Config} body
+ * @returns {TagConfig}
  */
 function tag(name, attributes, body) {
 	return { type: 'tag', name, attributes, body };
 }
 
 /**
- * Returns a string of a function returning the value.
+ * Represents a function for rendering.
  * @param {string[]} arguments
- * @param {Object} value
- * @returns {Object}
+ * @param {Config} value
+ * @returns {FuncConfig}
  */
 function func(arguments, value) {
 	return { type: 'func', arguments, value };
 }
 
 /**
- * Renders representations.
- * @param {Object} config
- * @param {Object} options
- * @param {string} options.type
- * @param {string} options.quote
- * @returns
+ * ...
+ * @typedef {Object} RenderOptions
+ * @property {string} type
+ * @property {string} quote
  */
 
-function render(config = {}, { type: esType = 'es6', quote } = {}) {
-	if (!quote) {
-		if (esType === 'es6') {
-			quote = '`';
-		} else {
-			quote = '\'';
-		}
-	}
+/**
+ * Renders representations.
+ * @param {Config} config
+ * @param {RenderOptions} [options={}]
+ * @returns {string}
+ */
+function render(config, options = {}) {
+	const { type: esType = 'es6', quote = (esType === 'es6') ? '`' : '\'' } = options;
 
 	const { type: configType } = config;
 	if (render[configType]) {
@@ -95,7 +155,13 @@ function render(config = {}, { type: esType = 'es6', quote } = {}) {
 	return '';
 }
 
-render['fix'] = function ({ value } = {}, { quote } = {}) {
+/**
+ * ...
+ * @param {FixConfig} config
+ * @param {RenderOptions} [options={}]
+ * @returns {string}
+ */
+render['fix'] = function ({ value }, { quote } = {}) {
 	value = String(value)
 		.replace(new RegExp(quote, 'g'), `\\${quote}`)
 		.replace(new RegExp('(?:\r\n|\r|\n)', 'g'), '\\n')
@@ -104,7 +170,13 @@ render['fix'] = function ({ value } = {}, { quote } = {}) {
 	return `${quote}${value}${quote}`;
 };
 
-render['exp'] = function ({ value } = {}, { type, quote } = {}) {
+/**
+ * ...
+ * @param {ExpConfig} config
+ * @param {RenderOptions} [options={}]
+ * @returns {string}
+ */
+render['exp'] = function ({ value }, { type } = {}) {
 	if (type === 'es6') {
 		return `\`\${${value}}\``;
 	}
@@ -112,7 +184,13 @@ render['exp'] = function ({ value } = {}, { type, quote } = {}) {
 	return `(${value})`;
 };
 
-render['expIf'] = function ({ condition, trueValue, falseValue } = {}, { type, quote } = {}) {
+/**
+ * ...
+ * @param {ExpIfConfig} config
+ * @param {RenderOptions} [options={}]
+ * @returns {string}
+ */
+render['expIf'] = function ({ condition, trueValue, falseValue }, { type, quote } = {}) {
 	const value = `${condition} ? ${render(trueValue, { type, quote })} : ${render(falseValue, { type, quote })}`
 
 	if (type === 'es6') {
@@ -122,7 +200,13 @@ render['expIf'] = function ({ condition, trueValue, falseValue } = {}, { type, q
 	return `(${value})`;
 };
 
-render['expMap'] = function ({ array, arguments, value, joinString } = {}, { type, quote } = {}) {
+/**
+ * ...
+ * @param {ExpMapConfig} config
+ * @param {RenderOptions} [options={}]
+ * @returns {string}
+ */
+render['expMap'] = function ({ array, arguments, value, joinString }, { type, quote } = {}) {
 	let values = [];
 
 	values.push(`(${array}).map(`);
@@ -162,7 +246,13 @@ render['expMap'] = function ({ array, arguments, value, joinString } = {}, { typ
 	return values;
 };
 
-render['cat'] = function ({ values } = {}, { type, quote } = {}) {
+/**
+ * ...
+ * @param {CatConfig} config
+ * @param {RenderOptions} [options={}]
+ * @returns {string}
+ */
+render['cat'] = function ({ values }, { type, quote } = {}) {
 	return values
 		.filter((value) => value != null)
 		.map((value) => render(value, { type, quote }))
@@ -170,7 +260,13 @@ render['cat'] = function ({ values } = {}, { type, quote } = {}) {
 		.replace(new RegExp(`${quote} \\+ ${quote}`, 'g'), '');
 };
 
-render['tag'] = function ({ name, attributes, body } = {}, { type, quote } = {}) {
+/**
+ * ...
+ * @param {TagConfig} config
+ * @param {RenderOptions} [options={}]
+ * @returns {string}
+ */
+render['tag'] = function ({ name, attributes, body }, { type, quote } = {}) {
 	const values = [];
 	values.push(fix('<'), name);
 	if (attributes != null) {
@@ -184,7 +280,13 @@ render['tag'] = function ({ name, attributes, body } = {}, { type, quote } = {})
 	return render(cat(...values), { type, quote });
 };
 
-render['func'] = function ({ arguments, value } = {}, { type, quote } = {}) {
+/**
+ * ...
+ * @param {FuncConfig} config
+ * @param {RenderOptions} [options={}]
+ * @returns {string}
+ */
+render['func'] = function ({ arguments, value }, { type, quote } = {}) {
 	const values = [];
 
 	if (type === 'es6') {
